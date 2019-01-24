48e8782ccc4f7c8e3c1a434ac19ae7b677c15a42
Zac Crosby <Zacros7164@gmail.com>
2019-01-24 14:31:46 -0500

    added sql queries and rendered response to screen

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,42 @@ connection.connect();
 console.log('i connnected')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const selectQuery = 'SELECT * FROM tasks;';
+  connection.query(selectQuery,(err,results)=>{
+    console.log(results)
+    res.render('index', { 
+      taskArray: results,
+    });
+  })
 });
+
+router.post('/addItem', function(req,res,next) {
+  const newTask = req.body.newTask;
+  const newTaskDate = req.body.newTaskDate;
+  // we know what the user submitted in the form data
+  // it comes to this route
+  // we store those values as a var, and then insert them into mySQL
+
+  // this is bad because of SQL injections
+  // const insertQuery = `INSERT INTO tasks (taskName, taskDate)
+  // VALUES
+  // ('${newTask}',${newTaskDate});`;
+  // console.log(insertQuery)
+
+  // SQL injection is when a user inserts a SQL statement into a form
+  // to run SQL when the dev didn't intend
+
+  const insertQuery = `INSERT INTO tasks (taskName, taskDate)
+    VALUES
+    (?,?);`;
+
+  connection.query(insertQuery,[newTask,newTaskDate],(err, results)=>{
+    if(err){
+      throw err;
+    }else{
+      res.redirect('/');
+    }
+  })
+})
 
 module.exports = router;
