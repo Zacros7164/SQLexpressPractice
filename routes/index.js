6f24117e5917b342e7772e707519a720e5b3ceff
Zac Crosby <Zacros7164@gmail.com>
2019-01-24 13:21:10 -0500

    init commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+const config = require('../config');
+var connection = mysql.createConnection(config);
+connection.connect();
+console.log('i connnected')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
